3f9fd34555019e96d34f32e896592b20072d3013
diegolpedro <diegolpedro@gmail.com>
2018-05-08 21:29:04 -0300

    Agregada escritura del archivo CSV

--- python/gcToCSV.py
+++ python/gcToCSV.py
@@ -18,38 +18,44 @@ def displaymatch(regex, testLine):
     return 0
   return 1
 
+# Inicializacion de variables.
 folder_name = sys.argv[1] # Nombre del directorio recibido por
                           # linea de comandos.
 files = commands.getoutput('ls '+folder_name).split()
                           # Leemos contenido de directorio y separamos
                           # el resultado en forma de array.
                           # [file1, file2, ..., filen]
+idFile = 0
 
-file_out = open('output.csv', w)
+# Apertura de archivo de salida.
+file_out = open('output.csv', 'w')
+lineaCsv = 'id,filename,cantG1,cantM204'+'\n'
+file_out.write(lineaCsv)
+
 
 for file_name in files:   # Recorremos archivos.
   
   cantG1 = 0              # Inicializamos contadores de etiquetas.
   cantM204 = 0
-  cantLine = 0
-
+  
   path = folder_name+'/'+file_name # Armamos direccion.
   
   print path
 
   with open(path, 'r') as file:    # Abrimos archivos y contamos etiquetas.
+    idFile = idFile+1
     for line_text in file:
       cantG1 = cantG1+displaymatch("^G1", line_text)
       cantM204 = cantM204+displaymatch("^M204", line_text)
-      cantLine = cantLine+1
     file.close()
 
   # Mostramos resultados.
   print "Archivo: "+file_name
-  print "Cantidad de Lineas: "+str(cantLine)
+  print "Numero de Archivo: "+str(idFile)
   print "Cantidad de G1: "+str(cantG1)
   print "Cantidad de M204: "+str(cantM204)
-  lineaCsv = file_name+','+cantLine+','+cantG1+','+cantM204
+
+  lineaCsv = str(idFile)+','+file_name+','+str(cantG1)+','+str(cantM204)+'\n'
   file_out.write(lineaCsv)
   
 
